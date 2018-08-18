9da52afbfd0f408323093e52d8617b476c47cd6f
miguel jimenez <denkatalabeno@gmail.com>
2018-08-18 04:15:32 +0200

    added some cool animation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 console.log("hello world");
+const baseSRC = "./assets/svg/"
+
+const partyItems = ['candles', 'blower', 'disco', 'fireworks', 'horn']
+
+const partyImage = document.querySelector('.party-image')
 
 const kite = document.querySelector('.kite');
-
 const harness = document.querySelector('.harness');
 const surfboard = document.querySelector('.surfboard');
 console.dir(kite);
@@ -23,3 +27,8 @@ window.addEventListener('scroll', (event)=> {
     }
     
 })
+
+setInterval(()=> {
+    let newImage = partyItems[Math.floor(Math.random() * partyItems.length)];
+    partyImage.src = baseSRC + newImage + '.svg';
+}, 1000)
